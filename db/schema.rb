bb21ef885038b441bca3e3dc0ebfc6fa1b26c234
car <c_chow568@yahoo.ca>
2016-03-22 18:38:11 -0700

    Fix merge conflict w/ pre-master

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160322183133) do
+ActiveRecord::Schema.define(version: 20160317195857) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "festival_genres", force: :cascade do |t|
-    t.integer  "genre_id"
-    t.integer  "festival_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "festival_genres", ["festival_id"], name: "index_festival_genres_on_festival_id"
-  add_index "festival_genres", ["genre_id"], name: "index_festival_genres_on_genre_id"
 
   create_table "festivals", force: :cascade do |t|
     t.string   "name"
@@ -43,13 +32,6 @@ ActiveRecord::Schema.define(version: 20160322183133) do
     t.string   "camping"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.date     "end_date"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "performances", force: :cascade do |t|
