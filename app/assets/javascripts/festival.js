d8e1c2070ff6d72f84a804b7f72b2cde0c62d010
lechos <linneasaltel@gmail.com>
2016-03-26 00:33:26 +0000

    header, card, pan, toggle

--- app/assets/javascripts/festival.js
+++ app/assets/javascripts/festival.js
@@ -1,48 +1,48 @@
 $(function() {
-  console.log("inside function")
-  var airports = function(request, response) {
-    $.getJSON("/autocomplete", {
-      query: request.term,
-    }, function(data) {
-      // data is an array of objects and must be transformed for autocomplete to use
-      var array = data.error ? [] : $.map(data, function(m) {
-        return {
-          label: m.name,
-          value: m.code
-        };
-      });
-      return response(array);
-    });
-  };
+  // console.log("inside function")
+  // var airports = function(request, response) {
+  //   $.getJSON("/autocomplete", {
+  //     query: request.term,
+  //   }, function(data) {
+  //     // data is an array of objects and must be transformed for autocomplete to use
+  //     var array = data.error ? [] : $.map(data, function(m) {
+  //       return {
+  //         label: m.name,
+  //         value: m.code
+  //       };
+  //     });
+  //     return response(array);
+  //   });
+  // };
 
-//  $("#departure_airport").autocomplete({
-//    delay: 500,
-//    minLength: 3,
-//    source: airports,
-//    select: function(event, ui) {
-//      $('#departure_airport').val(ui.item.value);
-//    }
-//  })
-//  .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-//            return $( "<li>" )
-//            .append( "<a>" + item.label + "<br>" + item.value + "</a>" )
-//            .appendTo( ul );
-//         };
-//
+ // $("#departure_airport").autocomplete({
+ //   delay: 500,
+ //   minLength: 3,
+ //   source: airports,
+ //   select: function(event, ui) {
+ //     $('#departure_airport').val(ui.item.value);
+ //   }
+ // })
+ // .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+ //           return $( "<li>" )
+ //           .append( "<a>" + item.label + "<br>" + item.value + "</a>" )
+ //           .appendTo( ul );
+ //        };
+
  
   // FLICKR
-  if ($('.festival-details').length > 0) {
-    var festival = $('.flickr-imgs').data('name');
-    $.ajax('/flickr_images/' + festival, { dataType: 'json' }).done(function(data) { 
-      if (data.stat != 'ok') { return console.log('error'); }
+  // if ($('.festival-details').length > 0) {
+  //   var festival = $('.flickr-imgs').data('name');
+  //   $.ajax('/flickr_images/' + festival, { dataType: 'json' }).done(function(data) { 
+  //     if (data.stat != 'ok') { return console.log('error'); }
       
-      var imgs = data.photos.photo;
-      imgs.forEach(function(img) { 
-        var imgSrc = 'https://farm'+img.farm+'.staticflickr.com/'+img.server+'/'+img.id+'_'+img.secret+'.jpg';
-        $('<img>').attr('src', imgSrc).appendTo('.flickr-imgs');
-      });
-    });
-  }
+  //     var imgs = data.photos.photo;
+  //     imgs.forEach(function(img) { 
+  //       var imgSrc = 'https://farm'+img.farm+'.staticflickr.com/'+img.server+'/'+img.id+'_'+img.secret+'.jpg';
+  //       $('<img>').attr('src', imgSrc).appendTo('.flickr-imgs');
+  //     });
+  //   });
+  // }
 
   console.log("outside function")
   window.initMap = function initMap() {
@@ -68,7 +68,7 @@ $(function() {
           position: {lat:festival.latitude, lng:festival.longitude}, 
           name: name
         });
-        var contentString = "This is a string";
+        // var contentString = "This is a string";
         var infowindow = new google.maps.InfoWindow({
           content: (festival.name + ',' + ' ' + festival.date)
         });
@@ -84,30 +84,29 @@ $(function() {
         })
       });
     });
-  
-    $('.pan').on('click', function(){
+
+    $(".map_button").click(function(){
+    $("#map").toggle(300);
+    });
+
+      $('.pan_button').on('click', function(){
       var latLng = new google.maps.LatLng(49.8994, -97.1392); //should pan to specified location (based on card/div?)
       map.panTo(latLng);
-    });
-  }
-    // var lastScrollTop = 0;
-    //   $(window).scroll(function() {
-    //   var st = $(this).scrollTop();
-    //     if (st < lastScrollTop) {
-    //       $('#wel').fadeIn();
-    //     } else {
-    //       $('#wel').fadeOut();
-    //     }
-    //   lastScrollTop = st;
-    // }
-    // })
-    $(window).on("scroll", function(e) {
-      if ($(window).scrollTop() >= $("#wel").height()) $("#wel").fadeOut(1000);
-      else $("#wel").fadeIn(1200);
-    // });
-  });
-});
+      });
+    }
+   
+    var target = $('#wel');
+    var targetHeight = target.outerHeight();
 
+    $(window).scroll(function(){
+      var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+      if(scrollPercent >= 0){
+        target.css('opacity', scrollPercent);
+      }
+    }); 
+  });
+
+// });
 
  // var map;
  //      function initMap() {
