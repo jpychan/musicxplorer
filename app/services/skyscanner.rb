7f7b6bfea9842ec4ce77ec363f4e24e627d41b38
jpychan <jennychan234@gmail.com>
2016-04-05 09:46:25 -0700

    added logging to see what's happening during flight search

--- app/services/skyscanner.rb
+++ app/services/skyscanner.rb
@@ -17,7 +17,9 @@ module Skyscanner
     request["accept"] = 'application/json'
     request["cache-control"] = 'no-cache'
     request.body = "country=CA&currency=CAD&locale=en-CA&adults=#{params[:adult]}&children=#{params[:children]}&infants=#{params[:infants]}&originplace=#{params[:departure_airport]}-iata&destinationplace=#{params[:arrival_airport]}-iata&outbounddate=#{outbound_date}&inbounddate=#{inbound_date}&locationschema=Iata&cabinclass=#{params[:cabin_class]}&groupPricing=true"
+    puts request.body
     response = http.request(request)
+    puts response
 
     if response.code != "201"
       session_id = nil
